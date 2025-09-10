1d644b5cac20655c1e3eaf14bd81563f165a5d2c
abiirjama <rd0387oi@go.minnstate.edu>
2025-09-10 21:48:43 +0000

    Initial Commit

---
+++ Module2_Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Module2_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user for the number of miles driven
+            Console.Write("enter the number of miles driven:");
+            double milesdriven = Convert.ToDouble(Console.ReadLine());
+
+            //ask the user for the number of gallons used 
+            Console.Write("enter the number of gallons of gasoline used: ");
+            double gallonsUsed = Convert.ToDouble(Console.ReadLine());
+            //calculate MPG using the formula:miles driven /gallons used
+            double mpg = milesdriven / gallonsUsed;
+
+            //display the result to the user
+            Console.WriteLine("your car's MPG is: " + mpg);
+
+        
+            
+        }
+    
+    }
+}
